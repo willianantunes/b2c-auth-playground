38b6a795845911682c41e09ba8be6219b5060b7a
Willian Antunes <willian.lima.antunes@gmail.com>
2021-09-23 12:12:26 -0300

    chore: add parcel support with jest (just in case)

---
+++ b2c_auth_playground/apps/core/static/core/js/controllers/HomeController.js
@@ -0,0 +1,19 @@
+import { $ } from "../utils/dom"
+
+export class HomeController {
+  constructor() {
+    // All inputs
+    // this._inputTextToBeTranscribed = $("textarea[name=text-to-be-transcribed]")
+    // this._inputPitch = $("input[name=pitch]")
+    // Buttons
+    // this._buttonPlayOrStop = $("a.play-tts")
+    // Values
+    // this._pitchValue = $(".pitch-value")
+    // Events
+    this._initAllEvents()
+  }
+
+  _initAllEvents() {
+    // Not need for it
+  }
+}
